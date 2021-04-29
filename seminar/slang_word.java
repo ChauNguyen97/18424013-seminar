317be6a59d0bf023926c7862a131841888ad3c4f
ChauNguyen97 <50213352+ChauNguyen97@users.noreply.github.com>
2021-04-29 23:39:28 +0700

    Add files via upload
    Đọc file slang.txt

---
+++ seminar/slang_word.java
@@ -0,0 +1,65 @@
+package seminar;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class slang_word {
+	
+	public static HashMap<String, List<String>> dictHashMap = new HashMap<String, List<String>>();
+	public static ArrayList<String> hisList = new ArrayList<String>();
+	public static Scanner sr = new java.util.Scanner(System.in);
+	
+	public static void ReadFile(String file_name) {
+        try {
+        	File file =new File(file_name);
+        	FileReader fileReader = new FileReader(file);
+        	
+        	BufferedReader bReader = new BufferedReader(fileReader);
+        	String line;
+        	while((line = bReader.readLine()) != null) {
+        		if(line.contains("`")) {
+        			List<String> tmpList = new ArrayList<String>();
+        			String[] s = line.split("`");
+        			if (s[1].contains("|")) {
+						String[] tmp = s[1].split("\\|");// "Welcome |to| java" -> [Welcome, to, java]
+						for(int i = 0; i < tmp.length; i++) {
+							tmp[i] = tmp[i].trim();
+						}
+						tmpList = Arrays.asList(tmp);
+					}
+        			else {
+						tmpList.add(s[1]);
+					}
+        			dictHashMap.put(s[0], tmpList);
+        		}
+        		
+            }
+        	fileReader.close();
+        	bReader.close();
+        }
+        catch (Exception ex) {
+            System.out.println("Error: "+ex);
+        }
+    }
+
+	public slang_word() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReadFile("slang.txt");
+		for (String name: dictHashMap.keySet()) {
+		    String key = name.toString();
+		    String value = dictHashMap.get(name).toString();
+		    System.out.println(key + " " + value);
+		}
+	}
+
+}
